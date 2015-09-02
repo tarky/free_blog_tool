af648bba0986f2314f1e4d8227384cc027459027
tarky <taisuke.araki@gmail.com>
2015-09-02 13:28:07 +0900

    add get_i2i_tag and add configure_blog

--- free_blogger/main.rb
+++ free_blogger/main.rb
@@ -3,6 +3,7 @@ require 'pry'
 require "capybara"
 require "kramdown"
 require "nokogiri"
+require "retriable"
 require "csv"
 require "yaml"
 $config = YAML.load_file(File.expand_path('../.config.yml', __FILE__))
@@ -10,7 +11,7 @@ $config = YAML.load_file(File.expand_path('../.config.yml', __FILE__))
 include Capybara::DSL
 
 class FreeBlogger
-  attr_reader  :id, :login_id, :password, :title, :articles
+  attr_reader  :id, :login_id, :password, :title, :articles, :i2i_tag
 
   def initialize(id)
     @id       = id
@@ -18,6 +19,7 @@ class FreeBlogger
     @login_id = blog["ログインID"]
     @password = blog["ブログパス"]
     @title    = blog["ブログ名"]
+    @url      = blog["ブログURL"]
     @articles = self.class.get_articles
   end
 
@@ -76,12 +78,7 @@ class FreeBlogger
     find('#mce_138').set(body)
     click_button 'OK'
     first('.input-save').click
-    begin
-      click_link '新規投稿'
-    rescue ::Selenium::WebDriver::Error::StaleElementReferenceError => e
-      puts 'out error'
-      retry
-    end
+    Retriable.retriable { click_link '新規投稿' }
   end
 
   def submit_in_batch
@@ -93,6 +90,67 @@ class FreeBlogger
       body =  doc.inner_html
       submit( title, body )
     end
+  end
+
+  def get_i2i_tag
+    visit        $config['url']['i2i']
+    fill_in      'loginId', with: $config['i2i']['user_id']
+    fill_in      'loginPw', with: $config['i2i']['password']
+    click_button 'ログイン'
+    click_link   'i2i WEBパーツ'
+    click_link   '管理画面'
+    find(:xpath, "//img[contains(@alt, '新規パーツ作成')]").click
+    fill_in      'title',   with: @title
+    click_button '作成する'
+    fill_in      'title',   with: @title
+    fill_in      'url',     with: @url
+    all('input[type=checkbox]').each { |checkbox| checkbox.click }
+    click_button '確認へ進む'
+    click_button '登録する'
+    click_button 'タグ発行画面'
+    @i2i_tag = find('#tag').value
+  end
+
+  def configure_blog
+    self.sign_in
+    Retriable.retriable { find(".navsettings a").click }
+    find(:xpath, "//img[contains(@alt, 'ブログ設定')]").click
+    find("#blog_ext__common_header_0").click
+    click_button "ブログ設定の変更"
+
+    Retriable.retriable { find(".navsettings a").click }
+    find(:xpath, "//img[contains(@alt, '記事設定')]").click
+    find("#article_setting__accept_comment_0").click
+    find("#article_setting__affiliate_link_0").click
+    f = open(File.expand_path('../ping_list.txt', __FILE__))
+    fill_in 'article_setting__update_ping', with: f.read
+    click_button "保存"
+
+    Retriable.retriable { find(".navsettings a").click }
+    find(:xpath, "//img[contains(@alt, '広告設定')]").click
+    find("#blog__ad_types_0").click
+    find("#blog_ext__force_ad_status_2").click
+    find("#blog_ext__viasearch_ad_2").click
+    click_button "保存"
+
+    Retriable.retriable { find(".navdesign a").click }
+    within("#submenu_pc") { click_link "コンテンツ" }
+    find(:xpath, "//li[contains(@data-dispatch_name, 'article_search')]/span[contains(@class, 'remove')]").click
+    find(:xpath, "//li[contains(@data-dispatch_name, 'blogclick')]/span[contains(@class, 'remove')]").click
+    find(:xpath, "//li[contains(@data-dispatch_name, 'blopita')]/span[contains(@class, 'remove')]").click
+    find(:xpath, "//li[contains(@data-dispatch_name, 'recent_comment')]/span[contains(@class, 'remove')]").click
+    find(:xpath, "//li[contains(@data-dispatch_name, 'rss_affiliate')]/span[contains(@class, 'remove')]").click
+    all(:xpath, "//li[contains(@data-dispatch_name, 'free')]")[0].
+      drag_to all(:xpath, "//li[contains(@data-dispatch_name, 'rdf_summary')]")[1]
+    find(:xpath, "//li[contains(@data-dispatch_name, 'rdf_summary')]/span[contains(@class, 'remove')]").click
+    find(:xpath, "//li[contains(@data-dispatch_name, 'free')]/a[contains(@class, 'edit')]").click
+    within_frame( find("#content_iframe")) do
+       fill_in "content__title", with: "アクセス解析"
+       fill_in "content_free__text", with: "accesssssss"
+       click_button "保存"
+    end
+    find(".close").click
+    click_button "保存"
   end
 end
 
